aa993c4a880e020c3c4b0611febd598678762979
Ghazi Majali <33878056+gmajali@users.noreply.github.com>
2018-12-18 17:22:52 +0200

    (feat) Add NavBar, SlideShow

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MyNavBar from './NavBar/MyNavBar';
 import './App.css';
+import SlideShow from './SlideShow/SlideShow';
 
 class App extends Component {
   state = {
@@ -37,20 +38,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MyNavBar />
+        <SlideShow />
         <p>{this.state.response}</p>
         <form onSubmit={this.handleSubmit}>
           <p>
