b9a66343f1e76e07c86637f62a3b632049dfeae6
AzharAlbakri <albakriazhar.work@gmail.com>
2018-12-29 13:45:53 +0200

    (refactor) refactor to knex

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.post('/charities',function(req,res){
 
 // ,\"${owner_id}\"
     // NOTE: insert post information to the database
-    dbOpt.Schema.query(charity, function(err, result) {
+    Schema.query(charity, function(err, result) {
       if (result) {
         res.send("charity added scusesfully ")
       } else {
@@ -146,79 +146,71 @@ app.post('/userCharities',function(req, res) {
   console.log(req.body,"usrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
   var owner_id = req.body.owner_id
   // ORDER BY date DESC
-  var query = `select * from charities  WHERE owner_id = \"${owner_id}\"`
-  dbOpt.Schema.query(query, function(err, result) {
-    if (result) {
-      // console.log('result',result)
-      res.send(result)
-    } else {
-      res.send(err)
-    }
-  })
+  dbOpt.getUserChar(req, res)
 });
 
 
 
   // get all charities 
  
-  app.get('/charities',function(req, res) {
-    // ORDER BY column1, column2, ... ASC|DESC;
-    // ORDER BY date DESC
-    var query = `select * from charities`
-    dbOpt.Schema.query(query, function(err, result) {
-      if (result) {
-        // console.log('result',result)
-        res.send(result)
-      } else {
-        res.send(err)
-      }
-    })
-  });
+//   app.get('/charities',function(req, res) {
+//     // ORDER BY column1, column2, ... ASC|DESC;
+//     // ORDER BY date DESC
+//     var query = `select * from charities`
+//     Schema.query(query, function(err, result) {
+//       if (result) {
+//         // console.log('result',result)
+//         res.send(result)
+//       } else {
+//         res.send(err)
+//       }
+//     })
+//   });
 
 
-  // Api to update charities
+//   // Api to update charities
 
-  app.put('/charities',function(req, res) {
+//   app.put('/charities',function(req, res) {
 
-    var id = req.body.id;
-    var description = req.body.description;
-    var name = req.body.name;
-    var location = req.body.location;
-    var amount = req.body.amount;
-    var owner_id = req.body.owner_id;
+//     var id = req.body.id;
+//     var description = req.body.description;
+//     var name = req.body.name;
+//     var location = req.body.location;
+//     var amount = req.body.amount;
+//     var owner_id = req.body.owner_id;
 
-    // (\"${id}\",\"${name}\",\"${amount}\",\"${description}\",\"${location}\",\"${owner_id}\")`
+//     // (\"${id}\",\"${name}\",\"${amount}\",\"${description}\",\"${location}\",\"${owner_id}\")`
 
-    var query = `UPDATE charities SET name=\"${name}\", amount= \"${amount}\", location= \"${location}\", description= \"${description}\" WHERE id=\"${id}\"`
-    dbOpt.Schema.query(query, function(err, result) {
-      if (result) {
-        // console.log('result',result)
-        res.send("update charities succ")
-      } else {
-        res.send(err)
-      }
-    })
-  });
+//     var query = `UPDATE charities SET name=\"${name}\", amount= \"${amount}\", location= \"${location}\", description= \"${description}\" WHERE id=\"${id}\"`
+//     Schema.query(query, function(err, result) {
+//       if (result) {
+//         // console.log('result',result)
+//         res.send("update charities succ")
+//       } else {
+//         res.send(err)
+//       }
+//     })
+//   });
 
 
-  // Api to delete charities
+//   // Api to delete charities
   
-  app.delete('/charities',function(req, res) {
-console.log("delete rout: ",req.body)
-    var id = req.body.id;
+//   app.delete('/charities',function(req, res) {
+// console.log("delete rout: ",req.body)
+//     var id = req.body.id;
 
-    // (\"${id}\",\"${name}\",\"${amount}\",\"${description}\",\"${location}\",\"${owner_id}\")`
+//     // (\"${id}\",\"${name}\",\"${amount}\",\"${description}\",\"${location}\",\"${owner_id}\")`
 
-    var query = `DELETE FROM charities WHERE id=\"${id}\"`
-    dbOpt.Schema.query(query, function(err, result) {
-      if (result) {
-        // console.log('result',result)
-        res.send("delete charities succ")
-      } else {
-        res.send(err)
-      }
-    })
-  });
+//     var query = `DELETE FROM charities WHERE id=\"${id}\"`
+//     Schema.query(query, function(err, result) {
+//       if (result) {
+//         // console.log('result',result)
+//         res.send("delete charities succ")
+//       } else {
+//         res.send(err)
+//       }
+//     })
+//   });
 
 //azhar
 
